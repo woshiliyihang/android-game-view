73ecd6d109a6b0714af6fbe76bc7ee85ce4436ed
jasonliyihang <mhh.you@163.com>
2017-05-20 19:52:11 +0800

    添加关节

--- AndroidGameView/app/src/main/java/gameview/com/sijienet/com/androidgameview/GameView.java
+++ AndroidGameView/app/src/main/java/gameview/com/sijienet/com/androidgameview/GameView.java
@@ -1,6 +1,7 @@
 package gameview.com.sijienet.com.androidgameview;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -13,6 +14,9 @@ import org.jbox2d.dynamics.ContactListener;
 import org.jbox2d.dynamics.World;
 import org.jbox2d.dynamics.contacts.ContactPoint;
 import org.jbox2d.dynamics.contacts.ContactResult;
+import org.jbox2d.dynamics.joints.DistanceJoint;
+import org.jbox2d.dynamics.joints.DistanceJointDef;
+import org.jbox2d.dynamics.joints.Joint;
 
 import gameview.com.sijienet.com.androidgameviewbase.AndroidGameViewBase;
 
@@ -37,6 +41,9 @@ public class GameView extends AndroidGameViewBase implements View.OnClickListene
     private Body polygonForLine;
     private Body circleDef;
     private CircleGameObj circleGameObj;
+    private Body topDef;
+    private Body bottomDef;
+    private DistanceJoint joint;
 
     public GameView(Context context) {
         super(context);
@@ -80,6 +87,30 @@ public class GameView extends AndroidGameViewBase implements View.OnClickListene
         addGameObj(circleGameObj);
         circleGameObj.x=(screenWitdh-circleGameObj.width)/2;
         circleGameObj.y=0;
+        RectGameObj rectGameObj = new RectGameObj(getContext());
+        addGameObj(rectGameObj);
+        rectGameObj.x=0;
+        rectGameObj.y=300;
+        rectGameObj.width=200;
+        rectGameObj.height=10;
+        RectGameObj rectGameObj1 = new RectGameObj(getContext());
+        rectGameObj1.x=0;
+        rectGameObj1.y=250;
+        rectGameObj1.width=100;
+        rectGameObj1.height=10;
+        addGameObj(rectGameObj1);
+
+        RectGameObj rectGameObj2 = new RectGameObj(getContext());
+        rectGameObj2.x=100;
+        rectGameObj2.y=600;
+        rectGameObj2.width=100;
+        rectGameObj2.height=20;
+        addGameObj(rectGameObj2);
+
+        LineGameObj lineGameObj = new LineGameObj(getContext());
+        addGameObj(lineGameObj);
+        lineGameObj.index=8;
+
         useSort();
 
         //设置物理世界
@@ -95,11 +126,16 @@ public class GameView extends AndroidGameViewBase implements View.OnClickListene
         polygon2 = createPolygon(imgGameObj.x, imgGameObj.y, imgGameObj.width, imgGameObj.height, false);
         polygonForLine = createPolygonForLine( lineBody.x, lineBody.y, lineBody.width, lineBody.height, false);
         circleDef = createCircleDef(circleGameObj.x, circleGameObj.y, circleGameObj.radius, false);
+        topDef = createPolygon(rectGameObj1.x, rectGameObj1.y, rectGameObj1.width, rectGameObj1.height, false);
+        bottomDef = createPolygon(rectGameObj2.x, rectGameObj2.y, rectGameObj2.width, rectGameObj2.height, false);
+        addGameBodyBind(rectGameObj,createPolygon(rectGameObj.x,rectGameObj.y,rectGameObj.width,rectGameObj.height,true));
         addGameBodyBind(circleGameObj,circleDef);
         addGameBodyBind(imgGameObj,polygon2);
         addGameBodyBind(qiuGameObj,body);
-        addGameBodyBind(qiuGameObj2, polygon);
+        addGameBodyBind(qiuGameObj2, this.polygon);
         addGameBodyBind(lineBody,polygonForLine);
+        addGameBodyBind(rectGameObj1, topDef);
+        addGameBodyBind(rectGameObj2,bottomDef);
 
         //彭转检测
         imgGameObj.body.getShapeList().getFilterData().groupIndex=3;
@@ -108,9 +144,18 @@ public class GameView extends AndroidGameViewBase implements View.OnClickListene
         qiuGameObj2.body.getShapeList().getFilterData().categoryBits=4;
         qiuGameObj2.body.getShapeList().getFilterData().groupIndex=4;
 
-
+        //添加关节
+        addBodyJoin();
+        lineGameObj.distanceJoint=joint;
+        lineGameObj.rate=RATE;
 
         world.setContactListener(this);
+    }
+
+    private void addBodyJoin() {
+        DistanceJointDef jointDef=new DistanceJointDef();
+        jointDef.initialize(topDef,bottomDef,topDef.getWorldCenter(),bottomDef.getWorldCenter());
+        joint = (DistanceJoint)world.createJoint(jointDef);
     }
 
     @Override
@@ -124,6 +169,7 @@ public class GameView extends AndroidGameViewBase implements View.OnClickListene
             }
         });
     }
+
 
 
     @Override
@@ -144,6 +190,7 @@ public class GameView extends AndroidGameViewBase implements View.OnClickListene
     @Override
     public void onClick(View v) {
         imgGameObj.body.applyForce(new Vec2(0,-5000),imgGameObj.body.getWorldCenter());
+        bottomDef.applyForce(new Vec2(10,-1000),bottomDef.getWorldCenter());
         Log.i(tag,"touch");
     }
 
