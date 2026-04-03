b8dd16f358c5c2d2d3c1d4071d29f89f573607e5
json_li <mhh.you@163.com>
2026-04-03 11:22:01 +0800

    删除工程

--- AndroidGameView/lib_game_view/src/main/java/gameview/com/sijienet/com/androidgameviewbase/OnUpdateAnimer.java
+++
@@ -1,9 +0,0 @@
-package gameview.com.sijienet.com.androidgameviewbase;
-
-/**
- * 作者：李一航
- * 博客：http://sijienet.com/
- */
-public interface OnUpdateAnimer {
-    void run(int index);
-}
--- README.md
+++
@@ -1,8 +0,0 @@
-# android-game-view
-
-基于原生的Android游戏框架
-
-更新：
-
-1整合了jbox2d 物理引擎 。
-2原生Android开发。
